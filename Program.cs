7b4c961046cd960ea4a1f93379527cb19d74b8ba
richml1964 <47123551+richml1964@users.noreply.github.com>
2019-01-28 16:20:10 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 2;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Mars!");
+            int z = calculations.Add(i,j);
+            Console.WriteLine(z);
+        }
+        /*
+        public static int add(int i, int j)
+        {
+            return (i + j);
+        }
+        */
+    }
+}
---
+++ add.cs
@@ -0,0 +1,11 @@
+﻿namespace myFirstProject
+{
+    static class calculations
+    { 
+
+         public static int Add(int i, int j)
+        {
+            return (i + j);
+        }
+    }
+}
